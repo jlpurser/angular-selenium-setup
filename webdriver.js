0ea58e61b5ec26c2d90e33363814ff240cf86201
Jeremy Purser <jeremypurser@gmail.com>
2019-12-15 14:58:04 -0500

    Add driver command

--- webdriver.js
+++ webdriver.js
@@ -2,3 +2,14 @@ const webdriver = require("selenium-webdriver");
 const driver = new webdriver.Builder().forBrowser("chrome").build();
 
 driver.get("http://localhost:4200/");
+driver.wait(
+  webdriver.until.elementLocated({
+    className: "card-container"
+  })
+);
+
+driver
+  .findElement({
+    className: "card-container"
+  })
+  .click();
